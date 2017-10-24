e6bcbaa09cd881cfc5cda1b152bd457dd77930c6
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-24 21:25:02 +0200

    committing html and js files

---
+++ jsfile.js
@@ -0,0 +1,26 @@
+var myArray_1 = [1, 2, 3]
+console.log(myArray_1[1])
+
+
+var sleepy = true;
+if (sleepy) {
+    console.log('I am going to bed');
+} else {
+    console.log('Party party party!');
+}
+
+
+var myArray_2 = ['Thomas', 'Amber', 'Raoul'];
+var emptyArray = [];
+
+for (var i = 0; i < myArray_2.length; i++) {
+  emptyArray.push(myArray_2[i]);
+}
+console.log(emptyArray);
+
+
+function myFirstName(name) {
+  console.log('My first name is ' + name);
+}
+
+myFirstName('Zuzanna');
